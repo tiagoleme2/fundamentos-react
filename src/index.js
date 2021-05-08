e8caa1feb70832c0215d676af130b9e03bebc97e
Tiago Assis <tiago_assis2@hotmail.com>
2021-05-07 23:45:37 -0300

    Parametros

--- src/index.js
+++ src/index.js
@@ -3,8 +3,15 @@ import React from 'react'
 import ReactDom from 'react-dom'
 
 import Primeiro from './components/Primeiro'
+import ComParamentro from './components/ComParamentro'
 
 ReactDom.render(
-    <Primeiro></Primeiro>,
+    <div>
+        <Primeiro />
+        <ComParamentro titulo="Esse é o titulo"
+        subtitulo="Esse é o subtitulo" />
+        <ComParamentro titulo="Opa"
+        subtitulo="Epa" />
+    </div>,
     document.getElementById('root')
 )
